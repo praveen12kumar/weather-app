6dfd05d9064a935c3121c91d9bcdba355408b57f
Praveen Kumar <praveenshakya441@gmail.com>
2025-01-08 01:08:27 +0530

    Refactor Weather API request

--- src/config/axiosInstance.tsx
+++ src/config/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from 'axios';
 
 const axiosInstance : AxiosInstance = axios.create({
-    baseURL: 'http://api.weatherapi.com/v1',
+    baseURL: 'https://api.weatherapi.com/v1',
     headers: {
         'Content-Type': 'application/json',
     },
